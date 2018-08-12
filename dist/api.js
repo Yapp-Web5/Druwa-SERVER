e9d1c53dd92367cc87772fabaf56d366055385b3
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-08-13 01:33:54 +0900

    Env: Install Tslint

--- dist/api.js
+++ dist/api.js
@@ -1,9 +1,22 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+router.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    res.send({
+        message: "Success"
+    });
+}));
 exports.default = router;
 //# sourceMappingURL=api.js.map
--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,10 @@
 import express from "express";
 const router = express.Router();
 
+router.get("/", async (req: express.Request, res: express.Response) => {
+  res.send({
+    message: "Success"
+  });
+});
+
 export default router;
