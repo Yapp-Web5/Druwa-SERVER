3ae9e4523c8d5b1fc3eb5b6500bf4a37e48b9cf4
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-08-17 02:14:47 +0900

    Env: Remove dist directory

--- dist/configs/session.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sessionConfig = {
-    secret: "123!@#",
-    resave: false,
-    saveUninitialized: true
-};
-exports.default = sessionConfig;
-//# sourceMappingURL=session.js.map
