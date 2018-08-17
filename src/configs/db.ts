92d60be01758b11e7e49712bc2c34cc442fe1a52
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-08-17 13:21:10 +0900

    Env: Add notice on DB config

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,2 +1,26 @@
-import { DB_ADMIN_USERNAME, DB_ADMIN_PASSWORD } from "./_db_account";
-export const DB_END_POINT = `mongodb://${DB_ADMIN_USERNAME}:${DB_ADMIN_PASSWORD}@ds123822.mlab.com:23822/druwa`;
+/*
+
+  Notice! You must create '_db_account.ts' file in this directory to operate server
+
+  example) 
+  : _db_account.ts
+
+  export const DB_ADMIN_USERNAME = OUR_MONGODB_USERNAME;
+  export const DB_ADMIN_PASSWORD = OUR_PASSWORD;
+  export const DB_URL = OUR_DB_URL;
+
+  --------
+  
+  If you forget OUR_MONGODB_USERNAME, OUR_PASSWORD or OUR_DB_URL, 
+  please send email to nayunhwan.dev@gmail.com or contact Yunhwan Na.
+
+  Because '_db_account.ts' file includes sensitive data related with Database. 
+  After creating '_db_account.ts' file, Never upload '_db_account.ts' file to 
+  any Github repository including Druwa-SERVER. 
+
+  Basically '_db_account.ts' is registered in .gitignore.
+
+*/
+
+import { DB_ADMIN_USERNAME, DB_ADMIN_PASSWORD, DB_URL } from "./_db_account";
+export const DB_END_POINT = `mongodb://${DB_ADMIN_USERNAME}:${DB_ADMIN_PASSWORD}@${DB_URL}`;
