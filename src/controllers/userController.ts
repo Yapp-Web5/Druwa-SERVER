0d77d46387280e44a9e843c114cea357d15a76a1
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-09-29 15:45:16 +0900

    UserController: Create get Username API and Create User API
    See: Github issue #6

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,17 +1,26 @@
 import express from "express";
 import * as crypto from "crypto-js";
 import { User, UserModel } from "../models/UserModel";
+import { generateNickname } from "../consts/nickname";
 
 const router = express.Router();
 
+router.get("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const token = req.headers.token;
+    const data = await UserModel.findOne({ token });
+    return res.send(data);
+  } catch (err) {
+    return res.status(404).send(err);
+  }
+});
+
 router.post("/", async (req: express.Request, res: express.Response) => {
   try {
-    const { username, email } = req.body;
     const token = crypto.SHA256(new Date().toString()).toString();
     const userObject = {
       token,
-      username: username as string,
-      email: email as string,
+      username: generateNickname(),
       createdAt: new Date(),
     } as User;
     const user = new UserModel(userObject);
