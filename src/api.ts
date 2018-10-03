5f2712b79c2982ce983f556e07a86109f5dcc19b
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-10-03 12:46:39 +0900

    TestController: Create Test Controller

--- src/api.ts
+++ src/api.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import roomController from "./controllers/roomController";
 import userController from "./controllers/userController";
+import testController from "./controllers/testController";
 
 const router = express.Router();
 
 router.use("/rooms", roomController);
 router.use("/users", userController);
+router.use("/test", testController);
 
 export default router;
