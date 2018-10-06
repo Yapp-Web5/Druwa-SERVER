55f2de563e703a5a412dec096dbf5f8e7e748527
Jinsun Park <wlstjs7246@naver.com>
2018-10-07 02:16:19 +0900

    RootCommentController: Create RootCommentController:
    See: Github issue #24

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ import roomController from "./controllers/roomController";
 import userController from "./controllers/userController";
 import testController from "./controllers/testController";
 import commentController from "./controllers/commentController";
+import rootcommentController from "./controllers/rootcommentController";
 import likeController from "./controllers/likeController"
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.use("/rooms", roomController);
 router.use("/users", userController);
 router.use("/test", testController);
+router.use("/rootcomment", rootcommentController);
 router.use("/comment", commentController);
 router.use("/like", likeController);
 
--- src/models/RootComment.ts
+++ src/models/RootComment.ts
@@ -1,25 +1,47 @@
 import { UserSchema, User } from "./UserModel";
-import { Like, LikeSchema } from "./LikeModel";
-import { Schema, Document, model } from "mongoose";
+import { Like, LikeSchema } from './LikeModel';
+import { Schema, Document, model, Mongoose } from "mongoose";
+import { CommentSchema, Comment } from "./CommentModel";
 
 export interface RootComment extends Document {
-  author: User;
+  author: string;
   content: string;
   refpageidx: number;
   likes: Like[];
+  comments: Comment[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export const RootCommentSchema = new Schema({
   author: {
-    type: UserSchema,
+    type: String,
+    index: true,
     required: true,
   },
   content: {
+    index: true,
     type: String,
   },
   refpageidx: {
     type: Number,
+    index: true,
     required: true,
+  },
+  comments: [{
+    type: Schema.Types.ObjectId, 
+    ref:'comment',
+  }],
+  likes:[{
+    type: LikeSchema,
+  }],
+  createdAt: {
+    index: true,
+    type: Date,
+  },
+  updatedAt: {
+    index: true,
+    type: Date,
   },
 });
 
