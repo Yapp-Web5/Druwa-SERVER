52e565c8e066646de25a9849ab8288c5720e121a
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-10-27 18:52:40 +0900

    UserController: Update UserController to add 'enteredRoom' property

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,11 +1,13 @@
 import { Schema, Document, model } from "mongoose";
+import { Room } from "./RoomModel";
 
 export interface User extends Document {
   token: string;
   username: string;
   createdAt: Date;
-  email: string;
-  password: string | null;
+  email?: string;
+  password?: string;
+  enteredRoom: Room[];
 }
 
 export const UserSchema = new Schema({
@@ -34,6 +36,12 @@ export const UserSchema = new Schema({
     index: true,
     sparse: true,
   },
+  enteredRoom: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "room",
+    },
+  ],
 });
 
 export const UserModel = model<User>("user", UserSchema);
