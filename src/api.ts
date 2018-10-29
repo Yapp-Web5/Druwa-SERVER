9194058983becba725b9df6b7fcb39c2ffdfb09f
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-10-30 01:07:12 +0900

    env: Remove useless files

--- src/api.ts
+++ src/api.ts
@@ -4,8 +4,6 @@ import cardController from "./controllers/cardController";
 import commentController from "./controllers/commentController";
 import roomController from "./controllers/roomController";
 import userController from "./controllers/userController";
-import testController from "./controllers/testController";
-import likeController from "./controllers/likeController";
 import uploadController from "./controllers/uploadController";
 
 const router = express.Router();
@@ -14,8 +12,6 @@ router.use("/cards", cardController);
 router.use("/comments", commentController);
 router.use("/rooms", roomController);
 router.use("/users", userController);
-router.use("/test", testController);
-router.use("/like", likeController);
 router.use("/files", uploadController);
 
 export default router;
